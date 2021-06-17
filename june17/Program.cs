741d72ef2b4d88f7352c8d569375eeb8e1547cf9
MurphyHill <murphyyhill@gmail.com>
2021-06-17 19:50:04 +0300

    first

---
+++ june17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+class Example
+{
+    static char GetRandomChar()
+    {
+        string symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*(/)><|:}{[]";
+        Random r = new Random();
+        var index = r.Next(symbols.Length);
+        return symbols[index];
+    }
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Clear();
+      
+       
+        Random x = new Random();
+        
+        while (true)
+        {
+            int valueX = x.Next(0, 237);
+            int valueY = x.Next(0, 100);
+
+            Console.SetCursorPosition(valueX, valueY);
+            Console.WriteLine(GetRandomChar());
+        }
+    }
+}
